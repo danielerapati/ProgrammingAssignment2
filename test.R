d9a6f406aeb20425a57c7bd13749543a8136a2a2
Daniele Rapati <daniele.rapati@gmail.com>
2014-07-25 13:39:05 +0100

    Updated, refactored test script (now a bit too verbose)

--- test.R
+++ test.R
@@ -1,17 +1,27 @@
 ## Test file for cachematrix.R with sample usage
 source("cachematrix.R")
 
-o <- matrix(1:4,2,2)
-m <- makeCacheMatrix(o)
-# now there should be no inverse in cache
-m$getinverse()
-# compute inverse
-inv <- cacheSolve(m)
-# now there should be an inverse in cache
-m$getinverse()
-# the computed inverse should be the same as the inverse of the original matrix
-cacheSolve(m) == solve(o)
+# tests if makeCacheMatrix and cacheSolve do what they are supposed to
+test <- function(o) {
+    message("original matrix")
+    print(o)
 
+    m <- makeCacheMatrix(o)
+
+    message("now there should be no inverse in cache")
+    print(m$getinverse())
+
+    message("compute inverse")
+    inv <- cacheSolve(m)
+
+    message("now there should be an inverse in cache")
+    print(m$getinverse())
+
+    message("the computed inverse should be the same as the inverse of the original matrix")
+    print(cacheSolve(m) == solve(o))
+}
+
+test(matrix(1:4,2,2))
 
 # same test on a bigger random matrix
-M <- matrix(runif(81),9,9)
+test(matrix(runif(81),9,9))
